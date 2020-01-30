ac63ea24a020ba01116fbf5317c297c1d0aab821
François <Fgrclaberge@gmail.com>
2020-01-29 23:26:03 -0500

    Handled the options

--- src/Httpc.java
+++ src/Httpc.java
@@ -5,23 +5,45 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import cliparser.OptionDoesNotExistException;
 import cliparser.OptionsParser;
 
 public class Httpc {
-
+	private static Method method;
+	private static boolean verbose = false;
+	private static HashMap headers = new HashMap();
+	private static String content = "";
+	
 	public static void main(String[] args) {
 		OptionsParser parser = new OptionsParser(1);
 		parser.addOption("get", 0, (arguments) ->
-			System.out.println("Here")
+			method = Method.GET
 		);
-		
-		parser.addOption("-v", 1, (arguments) ->
-			System.out.println("doing -v: "+arguments[0])
+		parser.addOption("post", 0, (arguments) ->
+			method = Method.POST
 		);
+		parser.addOption("-v", 0, (arguments) ->
+			verbose = true
+		);
+		parser.addOption("-h", 1, (arguments) ->{
+			String header = arguments[0];
+			int colonIndex = header.indexOf(':');
+			String headerKey = header.substring(0, colonIndex);
+			String headerValue = header.substring(colonIndex+1);
+			headers.put(headerKey, headerValue);
+		}
+		);
+		parser.addOption("-d", 1, (arguments) ->
+			content = arguments[0]
+		);
+		parser.addOption("-f", 1,(arguments)->
+			System.out.println("file")
+		);
+	
 		try {
-			parser.parse(args);
+			String url = parser.parse(args)[0];
 		} catch (OptionDoesNotExistException e) {
 			e.printStackTrace();
 		}
@@ -51,5 +73,10 @@ public class Httpc {
 		}
 		return response.toString();
 	}
-
+	
+	public enum Method{
+		GET, POST
+	}
 }
+
+
--- src/cliparser/OptionsParser.java
+++ src/cliparser/OptionsParser.java
@@ -1,5 +1,6 @@
 package cliparser;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,16 +18,27 @@ public class OptionsParser {
 		options.put(optionName, option);
 	}
 	
-	public void parse(String[] arguments) throws OptionDoesNotExistException {
-		for(int i=0; i<arguments.length; i++) {
-			Option currentOption = options.get(arguments[i]);
+	public String[] parse(String[] arguments) throws OptionDoesNotExistException {
+		String[] programArguments = getProgramArguments(arguments); 
+		String[] programOptions = Arrays.copyOfRange(arguments, 0, arguments.length-nbArguments);
+		for(int i=0; i<programOptions.length; i++) {
+			Option currentOption = options.get(programOptions[i]);
 			if(currentOption == null) throw new OptionDoesNotExistException();
 			String[] optionArgs = new String[currentOption.getNbArguments()];
 			for(int j=0; j<currentOption.getNbArguments(); j++) {
-				optionArgs[j] = arguments[i+j+1];
+				optionArgs[j] = programOptions[i+j+1];
 			}
 			currentOption.getDelegate().optionCallback(optionArgs);
 			i = i + currentOption.getNbArguments();
 		}
+		return programArguments;
 	}
+	
+	private String[] getProgramArguments(String[] arguments) {
+		String[] programArguments = new String[nbArguments];
+		for(int i=0; i<nbArguments; i++) {
+			programArguments[i] = arguments[arguments.length-i];
+		}
+		return programArguments;
+	}	
 }
